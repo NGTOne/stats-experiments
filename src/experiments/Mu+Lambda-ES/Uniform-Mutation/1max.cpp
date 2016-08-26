cbf2e060a9461d4268f4901634f583dfadc84c7f
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-26 14:08:47 -0400

    [Experiments]: Added binary experiments for ES

---
+++ src/experiments/Mu+Lambda-ES/Uniform-Mutation/1max.cpp
@@ -0,0 +1,22 @@
+#include "objectives/discrete/binary/1maxFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new OneMaxFunction(32),
+		new MuPlusLambdaES(
+			new UniformCrossover(1),
+			new UniformMutation(0.05),
+			150
+		),
+		argv[1],
+		std::stoul(argv[2]),
+		0,
+		1000
+	);
+
+	exper.run();
+}
---
+++ src/experiments/Mu+Lambda-ES/Uniform-Mutation/H-IFF.cpp
@@ -0,0 +1,22 @@
+#include "objectives/discrete/binary/H_IFFFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new H_IFFFunction(32),
+		new MuPlusLambdaES(
+			new UniformCrossover(1),
+			new UniformMutation(0.05),
+			150
+		),
+		argv[1],
+		std::stoul(argv[2]),
+		0,
+		1000
+	);
+
+	exper.run();
+}
