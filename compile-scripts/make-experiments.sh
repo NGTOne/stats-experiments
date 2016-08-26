cda98d95bdd9e9e20dc6569be2014fb359564ed8
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-25 23:57:02 -0400

    [Compile-Script]: Fixed sed expression

--- compile-scripts/make-experiments.sh
+++ compile-scripts/make-experiments.sh
@@ -13,7 +13,7 @@ fi
 
 for experiment in $(find src/experiments/* -print | egrep "pp$")
 do
-	dirname=$(echo "$experiment" | sed 's/src\/experiments\/|\.cpp$//g')
+	dirname=$(echo "$experiment" | sed 's/src\/experiments\/\|\.cpp$//g')
 
 	g++ -c -g -Wall -std=gnu++0x -Iinclude -I/usr/local/include -I$EIGEN $experiment -o obj/experiments/$dirname.o
 	g++ -o experiments/$dirname obj/experiments/$dirname.o obj/StatsExperiment.o $(find obj/objectives -name "*.o")
