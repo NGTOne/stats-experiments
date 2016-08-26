9b6f9dc0259fd34a9eae816c26f139a37d2d0a46
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-25 22:49:37 -0400

    [Experiment-compilation script]: Updated regex

--- compile-scripts/make-experiments.sh
+++ compile-scripts/make-experiments.sh
@@ -5,7 +5,7 @@ mkdir experiments results
 
 for experiment in $(find src/experiments/* -print | egrep "pp$")
 do
-	dirname=$(echo "$experiment" | sed 's/src\/experiments\///g' | sed 's/\.cpp$//g')
+	dirname=$(echo "$experiment" | sed 's/src\/experiments\/|\.cpp$//g')
 
 	g++ -c -g -Wall -std=gnu++0x -Iinclude -I/usr/local/include -I/usr/local/include/eigen3 $experiment -o obj/experiments/$dirname.o
 	g++ -o experiments/$dirname obj/experiments/$dirname.o obj/StatsExperiment.o $(find obj/objectives -name "*.o")
