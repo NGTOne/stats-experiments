d7dcc215d7c7f3718c0f6f351f5cd4b5766e56c6
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-26 00:13:11 -0400

    [Compile-Scripts]: Actually got compilation script working

--- compile-scripts/make-experiments.sh
+++ compile-scripts/make-experiments.sh
@@ -6,15 +6,21 @@ mkdir experiments-to-run results
 if [[ "$(uname -s)" == "Linux" ]]
 then
 	EIGEN="/usr/include/eigen3"
+	SHARED="-lHierGA"
 elif [[ "$(uname -s)" == "Darwin" ]]
 then
 	EIGEN="/usr/local/include/eigen3"
+	SHARED="-L/usr/local/lib -lHierGA"
 fi
 
 for experiment in $(find src/experiments/* -print | egrep "pp$")
 do
-	dirname=$(echo "$experiment" | sed 's/src\/experiments\/\|\.cpp$//g')
+	dirname=${experiment/src\/experiments\//}
+	dirname=${dirname/\.cpp/}
+	#targetDir=${dirname/\/[^/]+/}
+	targetDir=$(echo "$dirname" | sed 's/\/[^/]\+$//')
 
+	mkdir -p experiments-to-run/$targetDir
 	g++ -c -g -Wall -std=gnu++0x -Iinclude -I/usr/local/include -I$EIGEN $experiment -o obj/experiments/$dirname.o
-	g++ -o experiments-to-run/$dirname obj/experiments/$dirname.o obj/StatsExperiment.o $(find obj/objectives -name "*.o")
+	g++ -o experiments-to-run/$dirname obj/experiments/$dirname.o obj/StatsExperiment.o $(find obj/objectives -name "*.o") $SHARED
 done
