33c5270de7a60c6fadbd8c7b4771cacb1ad8ef8c
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-26 12:12:53 -0400

    [Compilation Script]: Optimized link line a bit

--- compile-scripts/make-experiments.sh
+++ compile-scripts/make-experiments.sh
@@ -13,14 +13,15 @@ then
 	SHARED="-L/usr/local/lib -lHierGA"
 fi
 
+shared_objs="$(find obj/objectives -name "*.o") obj/StatsExperiment.o"
+
 for experiment in $(find src/experiments/* -print | egrep "pp$")
 do
 	dirname=${experiment/src\/experiments\//}
 	dirname=${dirname/\.cpp/}
-	#targetDir=${dirname/\/[^/]+/}
 	targetDir=$(echo "$dirname" | sed 's/\/[^/]\+$//')
 
 	mkdir -p experiments-to-run/$targetDir
 	g++ -c -g -Wall -std=gnu++0x -Iinclude -I/usr/local/include -I$EIGEN $experiment -o obj/experiments/$dirname.o
-	g++ -o experiments-to-run/$dirname obj/experiments/$dirname.o obj/StatsExperiment.o $(find obj/objectives -name "*.o") $SHARED
+	g++ -o experiments-to-run/$dirname obj/experiments/$dirname.o $shared_objs $SHARED
 done
