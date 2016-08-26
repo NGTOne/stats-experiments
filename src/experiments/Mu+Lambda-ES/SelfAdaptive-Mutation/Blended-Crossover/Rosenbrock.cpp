e8b2921e5c2029a3369e92627ef32464fc037dd2
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-26 11:55:47 -0400

    [Experiments]: Added another one

---
+++ src/experiments/Mu+Lambda-ES/SelfAdaptive-Mutation/Blended-Crossover/Rosenbrock.cpp
@@ -0,0 +1,22 @@
+#include "objectives/continuous/n-d/RosenbrockFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new RosenbrockFunction(32, -10, 10),
+		new MuPlusLambdaES(
+			new BlendedCrossover({0.3, 0.7}),
+			new SelfAdaptiveMutation(false),
+			150
+		),
+		argv[1],
+		std::stoul(argv[2]),
+		0,
+		50
+	);
+
+	exper.run();
+}
