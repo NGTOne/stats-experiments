1300d2ff6cd7136af4cc1b80185f7f6f5be4f5b3
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-26 01:13:03 -0400

    [Experiment]: Tweaked params

--- src/experiments/Mu+Lambda-ES/SelfAdaptive-Mutation/Blended-Crossover/Sphere.cpp
+++ src/experiments/Mu+Lambda-ES/SelfAdaptive-Mutation/Blended-Crossover/Sphere.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char* argv[]) {
 	StatsExperiment exper(
 		50,
-		new SphereFunction(32, -100000, 100000),
+		new SphereFunction(32, -1000, 1000),
 		new MuPlusLambdaES(
 			new BlendedCrossover({0.3, 0.7}),
 			new SelfAdaptiveMutation(false),
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
 		argv[1],
 		std::stoul(argv[2]),
 		0,
-		1
+		1000
 	);
 
 	exper.run();
