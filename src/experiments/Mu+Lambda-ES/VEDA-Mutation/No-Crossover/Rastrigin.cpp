751cb418158ea18aada2fcdb970b52336b23763a
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-26 15:15:07 -0400

    [ES/VEDA-Mutation]: Added no-crossover experiments

---
+++ src/experiments/Mu+Lambda-ES/VEDA-Mutation/No-Crossover/Rastrigin.cpp
@@ -0,0 +1,18 @@
+#include "objectives/continuous/n-d/RastriginFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new RastriginFunction(32),
+		new VEDA_ES(150),
+		argv[1],
+		std::stoul(argv[2]),
+		0,
+		260
+	);
+
+	exper.run();
+}
---
+++ src/experiments/Mu+Lambda-ES/VEDA-Mutation/No-Crossover/Rosenbrock.cpp
@@ -0,0 +1,18 @@
+#include "objectives/continuous/n-d/RosenbrockFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new RosenbrockFunction(32, -10, 10),
+		new VEDA_ES(150),
+		argv[1],
+		std::stoul(argv[2]),
+		0,
+		50
+	);
+
+	exper.run();
+}
