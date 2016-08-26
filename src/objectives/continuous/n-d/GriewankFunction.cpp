e3e7cf89e3a38069da4c6763975a2f777691a3d8
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-26 12:04:28 -0400

    [Griewank]: Fixed typing and div-by-zero bugs

--- src/objectives/continuous/n-d/GriewankFunction.cpp
+++ src/objectives/continuous/n-d/GriewankFunction.cpp
@@ -2,12 +2,12 @@
 #include <math.h>
 
 float GriewankFunction::checkFitness(Genome* genome) {
-	double sum = 0;
-	double product = 1;
+	double sum = 0.0;
+	double product = 1.0;
 	for (unsigned int i = 0; i < this->genomeLength; i++) {
 		double value = genome->getIndex<double>(i);
 		sum += pow(value, 2);
-		product *= cos(value/sqrt(i));
+		product *= cos(value/sqrt((double)i+1.0));
 	}
-	return -(1 + (1/4000) * sum - product);
+	return -(1.0 + (1.0/4000.0) * sum - product);
 }
