2c6128c6347052926218471f5a45dfacc8e95a9e
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-27 14:20:18 -0400

    [ES/VEDAMutation]: Added balanced versions of crossovers

---
+++ src/experiments/Mu+Lambda-ES/VEDA-Mutation/Uniform-Crossover-Balanced/F8F2.cpp
@@ -0,0 +1,21 @@
+#include "objectives/continuous/n-d/F8F2Function.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new F8F2Function(32),
+		new VEDA_ES(
+			150,
+			new UniformCrossover(1)
+		),
+		argv[1],
+		std::stoul(argv[2]),
+		0,
+		30
+	);
+
+	exper.run();
+}
---
+++ src/experiments/Mu+Lambda-ES/VEDA-Mutation/Uniform-Crossover-Balanced/Griewank.cpp
@@ -0,0 +1,21 @@
+#include "objectives/continuous/n-d/GriewankFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new GriewankFunction(32, -1000, 1000),
+		new VEDA_ES(
+			150,
+			new UniformCrossover(1)
+		),
+		argv[1],
+		std::stoul(argv[2]),
+		0,
+		20
+	);
+
+	exper.run();
+}
