114a5409d230d7d7523d948ed36abc225882f400
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-27 14:17:17 -0400

    [ES/UniformMutation]: Added weighted version of crossover

---
+++ src/experiments/Mu+Lambda-ES/Uniform-Mutation/Uniform-Crossover-Weighted/AvgFrag.cpp
@@ -0,0 +1,22 @@
+#include "objectives/discrete/binary/AverageFragmentFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new AverageFragmentFunction(32),
+		new MuPlusLambdaES(
+			new UniformCrossover(1, {0.3, 0.7}),
+			new UniformMutation(0.05),
+			150
+		),
+		argv[1],
+		std::stoul(argv[2]),
+		32,
+		1
+	);
+
+	exper.run();
+}
---
+++ src/experiments/Mu+Lambda-ES/Uniform-Mutation/Uniform-Crossover-Weighted/LeadingOnes.cpp
@@ -0,0 +1,22 @@
+#include "objectives/discrete/binary/LeadingOnesFunction.hpp"
+#include "StatsExperiment.hpp"
+#include <libHierGA/HierGA.hpp>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	StatsExperiment exper(
+		50,
+		new LeadingOnesFunction(32),
+		new MuPlusLambdaES(
+			new UniformCrossover(1, {0.3, 0.7}),
+			new UniformMutation(0.05),
+			150
+		),
+		argv[1],
+		std::stoul(argv[2]),
+		32,
+		0
+	);
+
+	exper.run();
+}
